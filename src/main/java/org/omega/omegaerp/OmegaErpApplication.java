13ad71b82a028fd9a78229723daed9f2851b9fe8
Zemichale Tesfaye <zzimickwork@gmail.com>
2018-12-26 11:34:03 +0300

    initial commit

---
+++ src/main/java/org/omega/omegaerp/OmegaErpApplication.java
@@ -0,0 +1,14 @@
+package org.omega.omegaerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmegaErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmegaErpApplication.class, args);
+	}
+
+}
+
