4f8fb9f6a145eb56e9bedf183238549efd66d489
Zemichale Tesfaye <zzimickwork@gmail.com>
2018-12-26 13:22:17 +0300

    user entity merged

--- src/main/java/org/omega/omegaerp/models/User.java
+++ src/main/java/org/omega/omegaerp/models/User.java
@@ -1,29 +1,21 @@
 package org.omega.omegaerp.models;
 
 import javax.persistence.Entity;
-<<<<<<< HEAD
 import javax.persistence.OneToMany;
 import java.util.List;
+import javax.persistence.*;
+import java.util.Date;
 
 /**
  * Created by Quatra on 12/26/2018.
  */
-@Entity
-public class User {
-
-
-
-    @OneToMany
-    private List<Privilege> privileges;
-=======
-import javax.persistence.*;
-import java.util.Date;
-
 /**
  * Created by Kass on 12/26/2018.
  */
 @Entity
 public class User {
+
+
     @Id
     @GeneratedValue
     private int userId;
@@ -44,6 +36,9 @@ public class User {
     private boolean accountStatus;
     private String profileUrl;
     private boolean forceResetPassword;
->>>>>>> 14443a80cc4da24bd88880fcf49f3234d27a75df
+
+    @OneToMany
+    private List<Privilege> privileges;
+
 
 }
