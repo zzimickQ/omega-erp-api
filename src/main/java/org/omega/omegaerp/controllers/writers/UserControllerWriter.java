ded92fa8fd61cf25827b5d19049042d499238533
Zemichael Tesfaye <zzimick@gmail.com>
2019-01-16 12:03:56 -0600

    updated database certificate

--- src/main/java/org/omega/omegaerp/controllers/writers/UserControllerWriter.java
+++ src/main/java/org/omega/omegaerp/controllers/writers/UserControllerWriter.java
@@ -1,7 +1,14 @@
 package org.omega.omegaerp.controllers.writers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by Kass on 12/26/2018.
  */
+@RestController
+@RequestMapping
 public class UserControllerWriter {
+
+
 }
