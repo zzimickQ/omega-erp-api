4aab41f22109cac44632694bc4f2f7acdadb68b2
zzimickQ <zzimick@gmail.com>
2019-01-24 12:08:09 -0600

    HRM service examples updated

--- src/main/java/org/omega/hrm/services/ApplicantsService.java
+++ src/main/java/org/omega/hrm/services/ApplicantsService.java
@@ -1,7 +1,10 @@
 package org.omega.hrm.services;
 
 import org.omega.hrm.models.Applicants;
+import org.omega.hrm.models.Vacancy;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by Kass on 12/31/2018.
@@ -9,9 +12,13 @@ import org.springframework.stereotype.Service;
 @Service
 public abstract class ApplicantsService {
 
-    public abstract Applicants createApplicants(Applicants applicants);
+    public abstract Applicants registerApplication(Vacancy vacancy, Applicants applicant);
 
-    public abstract Applicants getApplicants();
+    public abstract List<Applicants> getApplicantsForVacancy(Vacancy vacancy);
+
+    public abstract List<Applicants> getApplicants();
+
+    public abstract Applicants getApplicant(Integer applicantId);
 
     public abstract void deleteApplicants(Integer id);
 
